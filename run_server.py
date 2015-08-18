cd9989ff6fb6879db2df1b704ce321ce2b7d588f
jeffen <foreverlovemichael@gmail.com>
2015-08-18 10:03:45 -0400

    create virtual environment

--- run_server.py
+++ run_server.py
@@ -1,4 +1,8 @@
 from repast import app
+import sys
+
+arglen = len(sys.argv)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=1234)
+    if arglen == 3:
+        app.run(host=sys.argv[1], port=int(sys.argv[2]))
