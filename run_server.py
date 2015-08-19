981d799448ba3c85ebda0d451077085ecad7ef4c
geoffen <foreverlovemichael@gmail.com>
2015-08-19 16:48:34 +0800

    update

--- run_server.py
+++ run_server.py
@@ -6,3 +6,5 @@ arglen = len(sys.argv)
 if __name__ == '__main__':
     if arglen == 3:
         app.run(host=sys.argv[1], port=int(sys.argv[2]))
+    else:
+    	print("Error args number, need ip and port");
